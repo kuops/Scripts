3a7550887b4c36cfc79aaab8c95c44998b489a56
kuops <opshsy@gmail.com>
2018-12-28 23:59:55 +0800

    Update wsl-init.sh

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -15,9 +15,12 @@ if ! grep 'WSL'  ~/.bashrc  &> /dev/null ;then
 	export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
 	export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH=/mnt/d/
 	export VAGRANT_HOME=/mnt/d/vagrant-home/.vagrant.d/
-	export GOROOT=$HOME/go
+	export GOROOT=/usr/local/go
 	export GOPATH=/mnt/c/Code/go_workspace
 	export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+	# X Server
+	export LIBGL_ALWAYS_INDIRECT=1
+	export DISPLAY=0:0
 
 	# my alias
 	alias  gohome='cd /mnt/c/Code/go_workspace'
@@ -28,13 +31,13 @@ fi
 
 # install golang
 curl -fSLO https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
-tar xf go1.11.1.linux-amd64.tar.gz
+tar xf go1.11.1.linux-amd64.tar.gz -C /usr/local/
 rm go1.11.1.linux-amd64.tar.gz
 
 # install vagrant
-wget https://releases.hashicorp.com/vagrant/2.1.5/vagrant_2.1.5_x86_64.deb
-sudo dpkg -i vagrant_2.1.5_x86_64.deb
-sudo rm vagrant_2.1.5_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.deb
+sudo dpkg -i vagrant_2.2.2_x86_64.deb
+sudo rm vagrant_2.2.2_x86_64.deb
 vagrant plugin install vagrant-hostmanager vagrant-env --plugin-clean-sources --plugin-source https://gems.ruby-china.com/
 
 # settings china apt-get sources
