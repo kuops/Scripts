e2d93ae5043726a2dcc8fe73c0ba640ed144f8f8
kuops <opshsy@gmail.com>
2018-12-29 01:43:01 +0800

    Update wsl-init.sh

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -78,6 +78,6 @@ sudo apt-get install -y tmux
 # upgrade soft
 sudo apt-get upgrade
 
-# install bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+# insall chinese pinyin
+sudo apt-get install fcitx
+sudo apt-get install fcitx-pinyin
