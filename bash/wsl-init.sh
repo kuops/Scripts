860bcb16ac19158f1b7f24248309a092d9d1da6b
kuops <opshsy@gmail.com>
2019-01-08 14:21:36 +0800

    Update wsl-init.sh

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -168,6 +168,9 @@ sudo apt-get install vim -y
 # remove vim
 # sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
 
+# install spacevim
+curl -sLf https://spacevim.org/install.sh | bash
+
 # install subl
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
