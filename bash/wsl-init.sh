070ee5a105fb4b801c6ad98d46d2d2f14438f9ca
kuops <opshsy@gmail.com>
2019-01-08 12:29:47 +0800

    Update wsl-init.sh

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -156,13 +156,14 @@ EOF
 chmod 600 /home/kuops/.ssh/config
 
 # install neovim
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-bash installer.sh ~/.cache/dein
+# sudo apt-get install software-properties-common
+# sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo apt-get update
+# sudo apt-get install neovim
+# sudo apt-get install python-dev python-pip python3-dev python3-pip
+# curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# bash installer.sh ~/.cache/dein
+# rm installer.sh -f
 
 # neovim setting
 # github address: https://github.com/Shougo/dein.vim
