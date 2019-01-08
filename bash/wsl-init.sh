2b7f73009ea67a14082a5429015d560763983afc
kuops <opshsy@gmail.com>
2019-01-08 13:45:12 +0800

    Update wsl-init.sh

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -161,6 +161,13 @@ chmod 600 /home/kuops/.ssh/config
 # github address: https://github.com/Shougo/dein.vim
 # https://github.com/vim-airline/vim-airline-themes
 
+# install vim
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt-get update
+sudo apt-get install vim -y
+# remove vim
+# sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
+
 # install subl
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
